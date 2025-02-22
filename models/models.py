0d4c3d7623107a0bbd9d3e97b6f5ab1db6646e81
DiovanePacheti <negociospacheti@gmail.com>
2025-02-22 20:57:17 -0300

    :art: Increasing character class snack

--- models/models.py
+++ models/models.py
@@ -19,8 +19,8 @@ class User(db.Model, UserMixin):
 
 class Snack(db.Model):
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
-    nome = db.Column(db.String, unique=True, nullable=False)
-    descricao = db.Column(db.String, unique=True)
+    name = db.Column(db.String(150), unique=True, nullable=False)
+    description = db.Column(db.String(150), unique=True)
     date = db.Column(db.DateTime, nullable=False)
     in_diet = db.Column(db.Boolean, nullable=False)
     users = db.relationship("User", secondary=user_snack, back_populates="snacks")
